038bc3bd74a3d9018a86a8d0580cb07bf5a2d566
Jedai Saboteur <paradox.inversion@gmail.com>
2023-02-08 00:43:14 -0800

    Added module index

---
+++ index.js
@@ -0,0 +1,16 @@
+const gameGenerators = require("./src/generators/game");
+const citizens = require("./src/citizens");
+const combat = require("./src/combat");
+const gameEvents = require("./src/gameEvents");
+const nations = require("./src/nations");
+const organizations = require("./src/organization");
+const utilities = require("./src/utilities");
+module.exports = {
+    gameGenerators,
+    citizens,
+    combat,
+    gameEvents,
+    nations,
+    organizations,
+    utilities
+}
