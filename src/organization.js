871193b2e03a88c5d9a54580bb3e3910e923e37c
Jedai Saboteur <paradox.inversion@gmail.com>
2023-03-11 10:26:11 -0800

    Add fns and jsdoc

--- src/organization.js
+++ src/organization.js
@@ -1,3 +1,4 @@
+const { generateAgentData } = require("./generators/game");
 const { throwErrorFromArray } = require("./utilities");
 
 /**
@@ -18,8 +19,8 @@ const recruitAgent = (organizationId, person, department=0) => {
 
 /**
  * 
- * @param {*} peopleArray 
- * @param {*} organizationId 
+ * @param {import("./typedef").Person[]} peopleArray 
+ * @param {string} organizationId 
  * @returns {import("./typedef").Person[]}
  */
 const getAgents = (peopleArray, organizationId) => {
@@ -32,9 +33,23 @@ const getAgents = (peopleArray, organizationId) => {
     return peopleArray.filter((person) => person.agent && person.agent.organizationId === organizationId);
 }
 
+const getAgentsInZone = (peopleArray, organizationId, zoneId) => {
+    const errors = [];
+    if (!organizationId){
+        errors.push("'organizationId' is a required parameter")
+    }
+    if (!zoneId){
+        errors.push("'zoneId' is a required parameter")
+    }
+    throwErrorFromArray(errors);
+
+    return peopleArray.filter((person) => person.agent && person.agent.organizationId === organizationId && person.homeZoneId === zoneId);
+}
+
 /**
  * Return the max number of agents an organization can support.
  * @param {import("./typedef").Person[]} peopleArray - An array of all people
+ * @param {string} organizationId
  */
 const getMaxAgents = (peopleArray, organizationId) => {
     return peopleArray.reduce((maxAgentValue, currentAgent) => {
@@ -47,6 +62,20 @@ const getMaxAgents = (peopleArray, organizationId) => {
     }, 0);
 }
 
+/**
+ * 
+ * @param {import("./typedef").Person[]} peopleArray 
+ * @param {import("./typedef").Person} agent 
+ */
+const getAgentSubordinates = (peopleArray, agent) => {
+    return peopleArray.filter((person)=> person.agent && person.agent.commanderId === agent.id)
+}
+
+/**
+ * Return the science value of an org.
+ * @param {import("./typedef").Person[]} peopleArray - An array of all people
+ * @param {string} organizationId
+ */
 const getScience = (peopleArray, organizationId) => {
     return getAgents(peopleArray, organizationId).reduce((science, currentAgent) => {
         if (currentAgent.agent.department === 2){
@@ -57,15 +86,47 @@ const getScience = (peopleArray, organizationId) => {
     }, 0);
 }
 
+/**
+ * Return the infra value of an org.
+ * @param {import("./typedef").Person[]} peopleArray - An array of all people
+ * @param {string} organizationId
+ */
 const getInfrastructure = (peopleArray, organizationId) => {
     
-    return getAgents(peopleArray, organizationId).reduce((science, currentAgent) => {
-        if (currentAgent.agent.department === 1){
-            return science + currentAgent.administration
+    return getAgents(peopleArray, organizationId).reduce((infrastructure, currentAgent) => {
+        if (currentAgent.agent.department === 1 || currentAgent.agent.department === 3){
+            return infrastructure + currentAgent.administration
         }
 
-        return science;
+        return infrastructure;
     }, 0);
+}
+
+const getPayroll = (peopleArray, organizationId) => {
+    return getAgents(peopleArray, organizationId).reduce((payroll, currentAgent) => {
+       
+        return payroll + currentAgent.agent.salary;
+    }, 0);
+}
+
+/**
+ * 
+ * @param {import("./typedef").Zone[]} zonesArray 
+ * @param {string} organizationId 
+ */
+const getControlledZones = (zonesArray, organizationId) => {
+    return zonesArray.filter(zone => zone.organizationId === organizationId);
+}
+
+
+const hireAgent = (agent, organizationId, department, commanderId, salary) => {
+    if (agent.agent){
+        console.log(`${agent.name} is already an agent.`)
+        return null;
+    }
+    const agentData = generateAgentData(organizationId, department, commanderId, salary);
+    agent.agent = agentData;
+    return agent;
 }
 
 module.exports = {
@@ -73,5 +134,10 @@ module.exports = {
     getAgents,
     getMaxAgents,
     getScience,
-    getInfrastructure
+    getInfrastructure,
+    getPayroll,
+    getAgentSubordinates,
+    getControlledZones,
+    hireAgent,
+    getAgentsInZone
 }
